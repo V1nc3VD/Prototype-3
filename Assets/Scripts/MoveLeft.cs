6d7c9c25caaab2b7daf923a7be405cb4e1f9e82f
Vince van Doorn <323888>
2019-12-15 16:21:52 +0100

    3.1 af

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private float speed = 30;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime*speed);
+    }
+}
